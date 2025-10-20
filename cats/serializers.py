1f42d917180198475d814b78e4f23108b5d03b64
vodoleikin <vodoleikin@yandex.ru>
2025-10-20 15:46:54 +0300

    20.10.2025. 15.45. v1. Work with API of the kittygram_project.

--- cats/serializers.py
+++ cats/serializers.py
@@ -6,4 +6,6 @@ from .models import Cat
 class CatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cat
-        fields = '__all__'
+        # fields = '__all__'
+        # fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('name', 'color', 'birth_year')
