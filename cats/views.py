9c9ba6ca1814c15b979aad460a27b6b7600cc436
vodoleikin <vodoleikin@yandex.ru>
2025-10-22 20:41:03 +0300

    22.10.2025. 20.40. v3. Work with API of the kittygram_project.

--- cats/views.py
+++ cats/views.py
@@ -68,19 +68,33 @@
 # ----------
 
 
-# Обновлённый views.py
-from rest_framework import generics
+# # Обновлённый views.py
+# from rest_framework import generics
 
+# from .models import Cat
+# from .serializers import CatSerializer
+
+
+# class CatList(generics.ListCreateAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+
+# class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+
+# ----------
+
+
+# views.py
+from rest_framework import viewsets
+ 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-class CatList(generics.ListCreateAPIView):
+class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-
-
-class CatDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Cat.objects.all()
-    serializer_class = CatSerializer
-
