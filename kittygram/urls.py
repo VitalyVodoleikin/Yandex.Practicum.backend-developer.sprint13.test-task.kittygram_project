df641d2a3c1a5cd6b8cf290b4be22898398848e8
vodoleikin <vodoleikin@yandex.ru>
2025-10-22 12:31:27 +0300

    22.10.2025. 13.30. v2. Work with API of the kittygram_project.

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,37 @@
+# from django.urls import path
+
+# from cats.views import cat_list
+
+# urlpatterns = [
+#    path('cats/', cat_list),
+# ]
+
+
+# ----------
+
+
+# # urls.py
+# from django.urls import path
+
+# from cats.views import APICat, APICatDetail
+
+# urlpatterns = [
+#     path('cats/', APICat.as_view()),
+#     path('cats/<int:pk>', APICatDetail.as_view()),
+# ]
+
+
+# ----------
+
+
+# Обновлённый urls.py
 from django.urls import path
 
-from cats.views import cat_list
+from cats.views import CatList, CatDetail
 
 urlpatterns = [
-   path('cats/', cat_list),
+    path('cats/', CatList.as_view()),
+    path('cats/<int:pk>/', CatDetail.as_view()),
 ]
 
 
